a5e02ee4325e5f6bb849500aa55b551fd52d988b
Kafelixon <piotrszkafel@gmail.com>
2023-05-18 22:22:26 +0200

    Refactor and  licence addition

--- README.md
+++ README.md
@@ -17,70 +17,69 @@ WebWatchNotify is an application designed to monitor specific files on websites 
 
 ### Prerequisites
 
+- Two Telegram bots with API keys
 - Python 3.x
 - pip
 
 ### Installation
 
 1. Clone the repository
-   ```
+   ```sh
    git clone https://github.com/username/WebWatchNotify.git
    ```
 2. Navigate into the cloned repository
-   ```
+   ```sh
    cd WebWatchNotify
    ```
 3. Install the dependencies
-   ```
+   ```sh
    pip install -r requirements.txt
    ```
-4. Configure the app by editing the `config.json` file. The configuration includes the website link, the steps to get to location of the file within the website's HTML, the Telegram Bot API key, and the target Telegram group or channel ID.
-   ```
+4. Configure the app by editing the `config.json` file. The configuration includes the website link, the steps to get to location of the file within the website's HTML, the Telegram Bots API keys, and the target Telegram group or channel ID.
+   ```json
    {
-       "website": "https://example.com",
-       "fileLocation": "/path/to/file",
-       "telegramBotApiKey": "your_telegram_bot_api_key",
-       "telegramGroupId": "your_telegram_group_id"
+     "configs": [
+       {
+         "name": "Example name",
+         "website": "https://example.com",
+         "steps": [
+           {
+             "method": "find_text",
+             "params": { "text": "Informatyka I stopnia  " }
+           },
+           { "method": "parent" },
+           { "method": "find_next_sibling" },
+           {
+             "method": "find_text",
+             "params": { "text": "Plan zajęć II semestr " }
+           },
+           { "method": "parent" },
+           { "method": "parent" },
+           { "method": "get_attribute", "params": { "name": "href" } }
+         ],
+         "bot_token": "your_telegram_bot_api_key",
+         "read_bot_token": "your_telegram_read_bot_api_key",
+         "chat_id": "your_telegram_group_id",
+         "schedule_interval": "1"
+       }
+     ]
    }
-   {
-  "configs": [
-    {
-      "name": "Example name",
-      "website": "https://example.com",
-      "steps": [
-        {
-          "method": "find_text",
-          "params": { "text": "Informatyka I stopnia  " }
-        },
-        { "method": "parent" },
-        { "method": "find_next_sibling" },
-        {
-          "method": "find_text",
-          "params": { "text": "Plan zajęć II semestr " }
-        },
-        { "method": "parent" },
-        { "method": "parent" },
-        { "method": "get_attribute", "params": { "name": "href" } }
-      ],
-      "telegramBotApiKey": "your_telegram_bot_api_key",
-      "telegramGroupId": "your_telegram_group_id"
-    }
-  ]
-}
-   ```
-5. Start the application
-   ```
-   python WebWatchNotify.py config.json
    ```
 
 ### Usage
 
 Once the app is running, it will start monitoring the file at the location specified in the `config.json` file within the website's HTML. When a change is detected, it will send a message to the specified Telegram group or channel.
 
+```sh
+   python3 WebWatchNotify.py config.json
+```
+
+There is check_once mode which will only check the file once and send a message if there is a change.
+
+```sh
+python3 WebWatchNotify.py config.json --check_once
+```
+
 ### Contributing
 
 Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
-
-### License
-
-[MIT](https://choosealicense.com/licenses/mit/)
--- WebWatchNotify.py
+++ WebWatchNotify.py
@@ -1,103 +1,171 @@
 import json
 import argparse
 import requests
-import re
-import time
-import schedule
 from bs4 import BeautifulSoup
+import schedule
+import time
 
 
-class FileWatcher:
-    """Class for watching for changes of files on a website"""
+class WebsiteFileWatcher:
+    """Class for monitoring file changes on a website and sending alerts via Telegram."""
 
-    def __init__(self):
+    def __init__(self, config_file: str):
+        """
+        Initialize the WebsiteFileWatcher with configurations from the specified file.
+        """
+        self.configs: list[dict] = self._load_configs(config_file)["configs"]
+        self.files = {}
+
+    @staticmethod
+    def _load_configs(config_file: str):
+        """
+        Load the configuration data from the JSON file.
+        """
         try:
-            with open("example.json") as f:
-                data = json.load(f)
-                configs = data["configs"]
+            with open(config_file) as f:
+                return json.load(f)
         except (OSError, json.JSONDecodeError) as e:
-            print("Cannot read the json config file properly\n", e)
+            print(f"Cannot read the json config file properly\n {e}")
             exit()
         except KeyError as e:
-            print("Cannot read json property", e)
+            print(f"Cannot read json property {e}")
             exit()
 
-        self.configs: list[dict] = configs
-        self.files: dict = {}
-
-    def scrape_website_for_file_url(self, config: dict) -> str:
+    def _perform_step(self, element: BeautifulSoup, step: dict) -> BeautifulSoup:
         """
-        Scrapes a website for a specific file URL based on a set of steps.
-
-        Parameters:
-        config (dict): A dictionary containing the website to scrape and the steps to perform. 
-                    The website is specified by the "website" key, 
-                    and the steps are specified by the "steps" key, which should be a list of steps.
-
-        Returns:
-        file_url (str): The file URL extracted from the website after performing all the steps
-                    specified in the config. The URL is returned as a string.
+        Perform a step on an element using BeautifulSoup as per the step configuration.
         """
+        step_methods = {
+            'find_text': lambda: element.find(string=step['params']['text']),
+            'parent': lambda: element.find_parent(),
+            'find_next_sibling': lambda: element.find_next_sibling(),
+            'get_attribute': lambda: element.get(step['params']['name']),
+        }
+        try:
+            return step_methods[step['method']]()
+        except KeyError:
+            raise ValueError(f'Invalid method: {step["method"]}')
 
+    def _scrape_website_for_file_url(self, config: dict) -> str:
+        """
+        Scrape a specific website for a file URL based on steps in the config.
+        """
         req = requests.get(config["website"])
         soup = BeautifulSoup(req.content, "html.parser")
 
         for step in config['steps']:
-            soup = self.perform_step(soup, step)
+            soup = self._perform_step(soup, step)
 
         return str(soup)
 
-    def check_and_update_files(self, config, element):
-        if config["name"] not in self.files or self.files[config["name"]] != element:
+    def _check_and_update_files(self, config, element):
+        """
+        Check the scraped file and update if there are any changes.
+        """
+        if self.files.get(config["name"]) != element:
             if config["name"] in self.files:
                 print("Changes detected")
+                self._send_file_to_telegram(config)
             self.files[config["name"]] = element
+        else:
+            print("No change in file detected")
 
     def scrape_websites(self):
+        """
+        Scrape all websites as per the loaded configurations.
+        """
         for config in self.configs:
-            file_url = self.scrape_website_for_file_url(config)
-            self.check_and_update_files(config, file_url)
+            file_url = self._scrape_website_for_file_url(config)
+            self._check_and_update_files(config, file_url)
 
-            print(self.files)
+    def _send_file_to_telegram(self, config: dict):
+        """
+        Send a file to a Telegram chat.
+        """
+        bot_token = config["bot_token"]
+        chat_id = config["chat_id"]
+        file_url = self.files[config["name"]]
+        url = f"https://api.telegram.org/bot{bot_token}/sendDocument?chat_id={chat_id}&document={file_url}"
 
-    def perform_step(self, element: BeautifulSoup, step: dict) -> BeautifulSoup:
-        if element is None:
-            raise ValueError(
-                'Element is None, check your steps. If you are looking for text, remember the spaces.')
-        method = step['method']
-        if method == 'find_text':
-            return element.find(string=step['params']['text'])
-        elif method == 'parent':
-            return element.find_parent()
-        elif method == 'find_next_sibling':
-            return element.find_next_sibling()
-        elif method == 'get_attribute':
-            return element.get(step['params']['name'])
+        response = requests.get(url)
+        if response.status_code == 200:
+            print("New file sent to Telegram")
         else:
-            raise ValueError(f'Invalid method: {method}')
+            print("Error sending file to Telegram")
+            print(response.text)
+
+    def read_last_message_from_telegram(self, config: dict) -> str:
+        """
+        Read the last message from a Telegram chat.
+        """
+        read_bot_token = config["read_bot_token"]
+        chat_id = config["chat_id"]
+        url = f"https://api.telegram.org/bot{read_bot_token}/getUpdates?chat_id={chat_id}"
+
+        response = requests.get(url)
+        if response.status_code != 200:
+            print("Error reading last message from Telegram")
+            print(response.text)
+            return ""
+
+        last_msg = response.json().get("result", [])[-1]
+        last_channel_post = last_msg.get("channel_post", {})
+        return last_channel_post.get("text") or last_channel_post.get("document", {}).get("file_name", "")
+
+    def check_for_file_changes(self):
+        for config in self.configs:
+            print("Checking file:", config)
+            print("Checking file:", config["name"])
+            last_msg = self.read_last_message_from_telegram(config)
+            file_name = self.files[config["name"]].split("/")[-1]
+            if last_msg != file_name:
+                print("Change in file detected")
+                self._send_file_to_telegram(config)
+            else:
+                print("No change in file detected")
+
+    def run_schedule(self):
+        """
+        Start the scheduler to scrape the websites at specified intervals.
+        """
+        print("Scheduler started")
+        for config in self.configs:
+            schedule.every(int(config["schedule_interval"])).minutes.do(
+                self.scrape_websites)
+
+        try:
+            while True:
+                schedule.run_pending()
+                time.sleep(1)
+        except KeyboardInterrupt:
+            print("\nProgram terminated by user. Bye!")
 
 
-def main():
-    # Initialize the ArgumentParser object
+def parse_arguments() -> argparse.Namespace:
+    """
+    Parse command-line arguments.
+    """
     parser = argparse.ArgumentParser()
-
-    # Add arguments
     parser.add_argument(
         "json_config", help="The name of the json_config file to process.")
     parser.add_argument("-co", "--check_once", action=argparse.BooleanOptionalAction, default=False,
                         help="Run check once, without scheduler.")
 
-    # Parse the arguments
-    args = parser.parse_args()
+    return parser.parse_args()
 
-    watcher = FileWatcher()
+
+def main():
+    """
+    Main function to start the file watcher and optionally the scheduler.
+    """
+    args = parse_arguments()
+    watcher = WebsiteFileWatcher(args.json_config)
     watcher.scrape_websites()
-    if not args.check_once:
-        print("Scheduler started")
-        schedule.every(1).minutes.do(watcher.scrape_websites)
-        while True:
-            schedule.run_pending()
-            time.sleep(1)
+    watcher.check_for_file_changes()
 
-if __name__ == '__main__':
+    if not args.check_once:
+        watcher.run_schedule()
+
+
+if __name__ == "__main__":
     main()
